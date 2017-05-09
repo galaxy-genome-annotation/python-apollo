cee1ccbfdb1ffb8cdb85218d97887132ec3b1bd7
Eric Rasche <rasche.eric@gmail.com>
2017-05-09 18:23:23 +0000

    Cleanup setup / deploy

---
+++ README.rst
@@ -0,0 +1,37 @@
+Apollo API Library
+==================
+
+Apollo is a Python library for interacting with
+`WebApollo <https://github.com/gmod/apollo/>`__
+
+Examples
+--------
+
+.. code:: python
+
+    from apollo import WebApolloInstance
+    wa = WebApolloInstance('https://fqdn/apollo', 'jane.doe@fqdn.edu', 'password')
+
+    orgs = wa.organisms.addOrganism(
+        "Yeast",
+        "/path/to/jbrowse/data",
+        genus='Saccharomyces',
+        species='cerevisiae',
+        public=False
+    )
+
+    # Give webapollo a second to process the uploaded organism.
+    time.sleep(1)
+
+    # Then add yourself to permission list
+    data = wa.users.updateOrganismPermission(
+        wa.me, "Yeast",
+        write=True,
+        export=True,
+        read=True,
+    )
+
+License
+-------
+
+Available under the MIT License
--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
-# http://bugs.python.org/issue15881#msg170215
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = open('README.rst').read()
+
 
 setup(
     name="apollo",
     version='1.0',
     description="WebApollo API library",
-    author="Eric Rasche",
-    author_email="esr@tamu.edu",
+    long_description=readme,
+    author="E Rasche",
+    author_email="hxr@hx42.org",
+    url='https://github.com/erasche/python-apollo',
+    packages=['apollo'],
     install_requires=['requests>=2.4.3'],
-    packages=find_packages(),
-    license='MIT',
-    platforms="Posix; MacOS X; Windows",
+    license="MIT",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7"
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ])
