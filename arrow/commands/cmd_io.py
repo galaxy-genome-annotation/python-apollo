b085bbaaabe7db22b2fd1831287aba522be3dc5d
Eric Rasche <rasche.eric@gmail.com>
2017-05-10 23:14:44 +0000

    Remove deprecated commands

--- arrow/commands/cmd_io.py
+++ arrow/commands/cmd_io.py
@@ -1,8 +1,7 @@
 import click
-from arrow.commands.io.write import cli as func0
-from arrow.commands.io.write_text import cli as func1
-from arrow.commands.io.download import cli as func2
-from arrow.commands.io.write_downloadable import cli as func3
+from arrow.commands.io.write_text import cli as func0
+from arrow.commands.io.download import cli as func1
+from arrow.commands.io.write_downloadable import cli as func2
 
 @click.group()
 def cli():
@@ -11,4 +10,3 @@ def cli():
 cli.add_command(func0)
 cli.add_command(func1)
 cli.add_command(func2)
-cli.add_command(func3)
