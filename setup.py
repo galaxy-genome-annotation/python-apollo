aa24b66efeea4aedf1b220187648a36340bac3a9
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2019-03-28 17:21:40 +0100

    add dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         [console_scripts]
         arrow=arrow.cli:arrow
     ''',
-    install_requires=['requests', 'biopython', 'cachetools', 'click>=6.7', 'wrapt', 'pyyaml'],
+    install_requires=['requests', 'biopython', 'cachetools', 'click>=6.7', 'wrapt', 'pyyaml', 'decorator'],
     license="MIT",
     classifiers=[
         "Development Status :: 4 - Beta",
