28312137aeecb0fd09ccc0599145881eaf827e11
Nathan Dunn <ndunn@me.com>
2020-05-15 15:57:43 -0700

    compliant with both docker and non-docker environment

--- bootstrap_apollo.sh
+++ bootstrap_apollo.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 SHOULD_LAUNCH_DOCKER=1
+ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/arrow.yml
+APOLLO_DATA_DIRECTORY="/data/"
+GALAXY_SHARED_DIR=`pwd`/apollo_shared_dir
 for arg in "$@"
 do
     case $arg in
         --nodocker)
         SHOULD_LAUNCH_DOCKER=0
+        ARROW_GLOBAL_CONFIG_PATH=`pwd`/test-data/local-arrow.yml
+        APOLLO_DATA_DIRECTORY=$GALAXY_SHARED_DIR
         shift
         ;;
         *)
@@ -14,7 +19,7 @@ do
     esac
 done
 
-export GALAXY_SHARED_DIR=`pwd`/apollo_shared_dir
+export ARROW_GLOBAL_CONFIG_PATH GALAXY_SHARED_DIR
 mkdir -p "$GALAXY_SHARED_DIR"
 
 if ! [[ $SHOULD_LAUNCH_DOCKER -eq 0 ]]; then
@@ -56,10 +61,10 @@ cp -r test-data/dataset_1_files/data/ "${GALAXY_SHARED_DIR}/org3"
 cp -r test-data/dataset_1_files/data/ "${GALAXY_SHARED_DIR}/org4"
 cp -r test-data/dataset_2_files/data/ "${GALAXY_SHARED_DIR}/org_update_newseq"
 cp -r test-data/dataset_3_files/data/ "${GALAXY_SHARED_DIR}/org_update_changedseq"
-arrow organisms add_organism --genus Testus --species organus test_organism /data/org1
-arrow organisms add_organism --genus Foo --species barus alt_org /data/org2
-arrow organisms add_organism --genus Foo3 --species barus org3 /data/org3
-arrow organisms add_organism --genus Foo4 --species barus org4 /data/org4
+arrow organisms add_organism --genus Testus --species organus test_organism $APOLLO_DATA_DIRECTORY/org1
+arrow organisms add_organism --genus Foo --species barus alt_org $APOLLO_DATA_DIRECTORY/org2
+arrow organisms add_organism --genus Foo3 --species barus org3 $APOLLO_DATA_DIRECTORY/org3
+arrow organisms add_organism --genus Foo4 --species barus org4 $APOLLO_DATA_DIRECTORY/org4
 
 # Give access to organisms for test user
 arrow users update_organism_permissions --write --read --export "test@bx.psu.edu" test_organism
