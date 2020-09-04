0d3079954490e8ab5ba0340e43332812ce8793ff
Cory Maughmer <cory.maughmer@tamu.edu>
2020-09-04 11:47:15 -0500

    4.2.9 bump, update_organism bugfix

--- apollo/organisms/__init__.py
+++ apollo/organisms/__init__.py
@@ -125,8 +125,11 @@ class OrganismsClient(Client):
         if suppress_output is not None and suppress_output is True:
             data['returnAllOrganisms'] = False
 
-        response = self.post('updateOrganismInfo', data)[0]
-
+        if suppress_output:
+            response = self.post('updateOrganismInfo', data)
+        else:
+            response = self.post('updateOrganismInfo', data)[0]
+           
         if len(response.keys()) == 0:
             return self.show_organism(organism_id)
         return response
